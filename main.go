88dfdf59ec12a3495b02191b25fdb16aeb7901f0
SiewTM <tuckmengsiew@gmail.com>
2021-06-04 09:47:40 +0800

    Update main.go
    update on 4th June 2021

--- main.go
+++ main.go
@@ -27,6 +27,23 @@ type User struct {
 	Password string
 }
 
+type Shop struct { // map this type to the record in the Shops table
+	ShopId      int
+	ShopName    string
+	ShopAddress string
+	ShopRating  string
+	ShopPeriod  string
+}
+
+type Item struct { // map this type to the record in the Shops table
+	ItemId    int
+	ItemName  string
+	ItemPrice float64
+	ItemDesc  string
+	ItemImg   string
+	ShopId    int
+}
+
 func main() {
 	db := myconnector.Connect()
 
@@ -34,6 +51,16 @@ func main() {
 		menu := []string{
 			"Register New User",
 			"Login",
+			"Get All Shops Details",
+			"Get Specific Shop Details",
+			"Add New Shop",
+			"Update Shop",
+			"Delete A Shop",
+			"Get All Items Details",
+			"Get Specific Item Details",
+			"Add New Item",
+			"Update Item",
+			"Delete An Item",
 		}
 
 		//switch
@@ -46,6 +73,21 @@ func main() {
 		//Login
 		var userName string
 		var userPassword string
+
+		//Shops Table
+		var idnew int
+		var namenew string
+		var addressnew string
+		var ratingnew string
+		var periodnew string
+
+		//Items Table
+		var iditem int
+		var nameitem string
+		var priceitem float64
+		var descitem string
+		var imgitem string
+		var idshop int
 
 		defer func() {
 			if err := recover(); err != nil {
@@ -112,6 +154,156 @@ func main() {
 			} else {
 				fmt.Println("Invalid username and/or password")
 			}
+
+		case 3:
+			//View All Shops
+			fmt.Println("1.Get All Shops Info")
+
+			myconnector.GetRecords(db)
+
+		case 4:
+			//Get Specific Shop Info
+			fmt.Println("2.Get Specific Shop Info")
+
+			fmt.Println("What is the specific Shop you want to view?")
+			fmt.Scanln(&idnew)
+
+			myconnector.GetSpecificRecord(db, idnew)
+
+		case 5:
+			//Add New Shop
+			fmt.Println("2.Add New Shop")
+
+			fmt.Println("Shop ID is auto generated?")
+			fmt.Scanln(&idnew)
+
+			fmt.Println("What is the new Shop Address?")
+			fmt.Scanln(&namenew)
+
+			fmt.Println("What is the new Shop Address?")
+			fmt.Scanln(&addressnew)
+
+			fmt.Println("What is the shop rating of the new shop?")
+			fmt.Scanln(&ratingnew)
+
+			fmt.Println("What is the shop availability period of the new shop?")
+			fmt.Scanln(&periodnew)
+
+			myconnector.InsertRecord(db, idnew, namenew, addressnew, ratingnew, periodnew)
+
+			fmt.Println(idnew, namenew, addressnew, ratingnew, periodnew, "is added.")
+
+		case 6:
+			//Update Shop
+			fmt.Println("4. Update Shop Details")
+
+			fmt.Println("What is the Shop To Be Updated?")
+			fmt.Scanln(&idnew)
+
+			fmt.Println("What is the Shop Name To Be Updated?")
+			fmt.Scanln(&namenew)
+
+			fmt.Println("What is the Shop Address To Be Updated?")
+			fmt.Scanln(&addressnew)
+
+			fmt.Println("What is the Shop Rating To Be Updated?")
+			fmt.Scanln(&ratingnew)
+
+			fmt.Println("What is the Shop Availability Period To Be Updated?")
+			fmt.Scanln(&periodnew)
+
+			myconnector.EditRecord(db, idnew, namenew, addressnew, ratingnew, periodnew)
+
+			fmt.Println(idnew, namenew, addressnew, ratingnew, periodnew, "are updated.")
+
+		case 7:
+			//Delete A Shop
+			fmt.Println("5. Delete A Shop")
+
+			fmt.Println("What is the Shop to be deleted?")
+			fmt.Scanln(&idnew)
+
+			myconnector.DeleteRecord(db, idnew)
+
+			fmt.Println(idnew, "is deleted.")
+
+		case 8:
+			//View All Items
+			fmt.Println("1.Get All Items Info")
+
+			myconnector.GetRecordsI(db)
+
+		case 9:
+			//Get Specific Item Info
+			fmt.Println("2.Get Specific Item Info")
+
+			fmt.Println("What is the specific Item you want to view?")
+			fmt.Scanln(&iditem)
+
+			myconnector.GetSpecificRecordI(db, iditem)
+
+		case 10:
+			//Add New Item
+			fmt.Println("2.Add New Item")
+
+			fmt.Println("Item ID is auto generated?")
+			fmt.Scanln(&iditem)
+
+			fmt.Println("What is the new Item Name?")
+			fmt.Scanln(&nameitem)
+
+			fmt.Println("What is the new Item Price?")
+			fmt.Scanln(&priceitem)
+
+			fmt.Println("What is the new Item Description?")
+			fmt.Scanln(&descitem)
+
+			fmt.Println("What is the new Item Image?")
+			fmt.Scanln(&imgitem)
+
+			fmt.Println("What is the new Shop ID To Be Updated?")
+			fmt.Scanln(&idshop)
+
+			myconnector.InsertRecordI(db, iditem, nameitem, priceitem, descitem, imgitem, idshop)
+
+			fmt.Println(idnew, namenew, addressnew, ratingnew, periodnew, "is added.")
+
+		case 11:
+			//Update Item
+			fmt.Println("4. Update Item Details")
+
+			fmt.Println("What is the Item ID To Be Updated?")
+			fmt.Scanln(&iditem)
+
+			fmt.Println("What is the Item Name To Be Updated?")
+			fmt.Scanln(&nameitem)
+
+			fmt.Println("What is the Item Price To Be Updated?")
+			fmt.Scanln(&priceitem)
+
+			fmt.Println("What is the Item Description To Be Updated?")
+			fmt.Scanln(&descitem)
+
+			fmt.Println("What is the Item Image To Be Updated?")
+			fmt.Scanln(&imgitem)
+
+			fmt.Println("What is the Shop ID To Be Updated?")
+			fmt.Scanln(&idshop)
+
+			myconnector.EditRecordI(db, iditem, nameitem, priceitem, descitem, imgitem, idshop)
+
+			fmt.Println(iditem, nameitem, priceitem, descitem, imgitem, idshop, "are updated.")
+
+		case 12:
+			//Delete An Item
+			fmt.Println("5. Delete An Item")
+
+			fmt.Println("What is the Item ID to be deleted?")
+			fmt.Scanln(&iditem)
+
+			myconnector.DeleteRecordI(db, iditem)
+
+			fmt.Println(iditem, "is deleted.")
 
 		default:
 			fmt.Println("Exit Program")
